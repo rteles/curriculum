daa2b2bf2b734abe1dcf23382e5c3046858d974a
Sthefanny <s.gonzaga@cinq.com.br>
2018-12-14 11:33:21 -0200

    Adding export option

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import './css/App.css';
 import Content from './components/content/content';
 import Sidebar from './components/sidebar/sidebar';
+import Export from './components/export';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Page">
-          <header className="App-header">
-            <Content />
-            <Sidebar />
-          </header>
-        </div>
+        <Export>
+          <div id="overallPage" className="Page">
+            <header className="App-header">
+              <Content />
+              <Sidebar />
+            </header>
+          </div>
+        </Export>
       </div>
     );
   }
