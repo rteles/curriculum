d1bc462d48cec886b08ee3aa4a411bf1c2d10db5
Roger Teles <r.teles@live.com>
2019-06-15 20:39:26 -0300

    Updates

---
+++ precache-manifest.119bcfef69e2c7d492a4a7862ba723ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/curriculum/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "33a94e889403d882dd75",
+    "url": "/curriculum/static/css/main.a82a092f.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/curriculum/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "5b19ca09f0602f703957",
+    "url": "/curriculum/static/js/1.5b19ca09.chunk.js"
+  },
+  {
+    "revision": "2fae871c0e353310d84d",
+    "url": "/curriculum/static/js/runtime~main.2fae871c.js"
+  },
+  {
+    "revision": "33a94e889403d882dd75",
+    "url": "/curriculum/static/js/main.33a94e88.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/curriculum/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/curriculum/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/curriculum/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "5b19ca09f0602f703957",
+    "url": "/curriculum/static/css/1.5eeab279.chunk.css"
+  },
+  {
+    "revision": "c083f6ec99d8ca7f558ab1faf1d0872f",
+    "url": "/curriculum/index.html"
+  }
+];
